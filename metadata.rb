6bb1f2d0f7c9a19128e4b4788a632e13351319fe
Jason Field <jason@avon-lea.co.uk>
2020-06-20 19:57:04 +0100

    Initial setup of repo

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'netplan'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+license          'Apache-2.0'
+description      'Installs/Configures netplan'
+source_url       'https://github.com/sous-chefs/netplan'
+issues_url       'https://github.com/sous-chefs/netplan/issues'
+chef_version     '>= 15.5'
+version          '0.1.0'
+
+supports 'ubuntu'
